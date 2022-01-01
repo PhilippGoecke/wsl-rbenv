ef3eae0f097982855be2194d3483dc2fc030aad2
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-01 20:54:10 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,10 +1,13 @@
 git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv --depth 1
 echo 'export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-#$HOME/.rbenv/bin/rbenv init
 rbenv init
 
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build --depth 1
 
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
+
+rbenv install -l
+rbenv install 3.1.0
+rbenv install -L
