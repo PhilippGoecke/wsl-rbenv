5790e00ac8dbb9c7889940c4641ab70df09ab8f0
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-01 20:42:15 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv --depth 1
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-export PATH="$HOME/.rbenv/bin:$PATH"
+echo 'export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
 $HOME/.rbenv/bin/rbenv init
 
 mkdir -p "$(rbenv root)"/plugins
