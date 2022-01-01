e0f87b0262790043867c6b80b727740c875aaafd
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-01 20:26:05 +0100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -2,4 +2,7 @@ git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 $HOME/.rbenv/bin/rbenv init
 
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | bash
